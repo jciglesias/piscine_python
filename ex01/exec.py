d7e439bdce988dca583b761217be4be6f51a2182
jciglesias <jciglesias@student.42.fr>
2022-11-10 17:48:27 +0100

    invert string, args and caps

---
+++ ex01/exec.py
@@ -0,0 +1,13 @@
+import sys
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        av = sys.argv;
+        av.reverse();
+        av.pop();
+        av = [x for x in av if x != '']
+        for i in range(len(av)):
+            av[i] = av[i][::-1].swapcase();
+        print(*av);
+    else:
+        print("Usage: python3 exec.py <Arg> ...");
