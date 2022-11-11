1edd11627c2bcca103c15a1d763b09daf0d2ea9a
jciglesias <jciglesias@student.42.fr>
2022-11-11 16:59:49 +0100

    remove words smaller than n and punctuations

---
+++ module00/ex07/filterwords.py
@@ -0,0 +1,15 @@
+import sys
+import string
+
+if __name__=="__main__":
+    argc = len(sys.argv);
+    if argc == 3 and sys.argv[2].isdigit():
+        arr: str = "";
+        for n in sys.argv[1]:
+            arr = arr + n.strip(string.punctuation);
+        arr = arr.split();
+        size = int(sys.argv[2]);
+        arr = [n for n in arr if len(n) > size];
+        print(arr);
+    else:
+        print("ERROR");
