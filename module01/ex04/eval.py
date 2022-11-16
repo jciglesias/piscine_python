15652c599b3fba75734a087ff05c82438ee20954
Juan Carlos <jiglesia@student.42.fr>
2022-11-16 22:33:21 +0100

    evaluate class to understand zip and enumerate

---
+++ module01/ex04/eval.py
@@ -0,0 +1,24 @@
+class Evaluator:
+    def zip_evaluate(coefs: list, words: list):
+        if type(coefs) is not list or type(words) is not list or len(words) != len(coefs):
+            return -1;
+        sum = 0;
+        for word, coef in zip(words, coefs):
+            if type(word) is not str:
+                return -1;
+            sum += len(word) * float(coef);
+        return sum;
+    def enumerate_evaluate(coefs: list, words: list):
+        if type(coefs) is not list or type(words) is not list or len(words) != len(coefs):
+            return -1;
+        sum = 0;
+        for i, (word, coef) in enumerate(zip(words, coefs)):
+            if type(word) is not str:
+                return -1;
+            sum += len(word) * float(coef);
+        return sum;
+
+if __name__=="__main__":
+    words = ["Le", "Lorem", "Ipsum", "est", "simple"]
+    coefs = [1.0, 2.0, 1.0, 4.0, 0.5]
+    print(Evaluator.enumerate_evaluate(coefs, words))
