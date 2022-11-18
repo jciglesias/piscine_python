1a8d18cf68d520b2f474199d9a9d05a26ceb9fc2
Juan Carlos <jiglesia@student.42.fr>
2022-11-18 19:25:31 +0100

    logger properly formated

--- module02/ex02/logger.py
+++ module02/ex02/logger.py
@@ -4,12 +4,21 @@ import os
 
 def log(fn):
     def inner(*args, **kwargs):
-        called_at = time.ctime()
-        to_execute = fn(*args, **kwargs)
+        action = {
+            "start_machine": "Start Machine",
+            "boil_water": "Boil Water",
+            "make_coffee": "Make Coffee",
+            "add_water": "Add Water"
+        }
+        init_time = time.perf_counter_ns()
+        ret = fn(*args, **kwargs)
+        exec_time = (time.perf_counter_ns() - init_time) / 1000000
+        time_mesure = "s" if exec_time > 999 else "ms"
+        if exec_time > 999: exec_time = exec_time / 1000
         fs = open("machine.log", "a")
-        fs.write('{0} executed. Logged at {1}\n'.format(fn.__name__, called_at))
+        fs.write('({})Running: {:19}[ exec-time = {:.3f} {} ]\n'.format(os.environ["USER"], action[fn.__name__], exec_time, time_mesure))
         fs.close()
-        return to_execute
+        return ret
     return inner
 
 class CoffeeMachine():
