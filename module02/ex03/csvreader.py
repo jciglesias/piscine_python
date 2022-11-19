f7504b6ffba002d340580620bb9843cb3566b4c0
Juan Carlos <jiglesia@student.42.fr>
2022-11-19 01:57:19 +0100

    init csvreader

---
+++ module02/ex03/csvreader.py
@@ -0,0 +1,25 @@
+class CsvReader():
+    def __init__(self, filename = None, sep = ',', header = False, skip_top = 0, skip_bottom = 0):
+        self.filename = filename
+        self.sep = sep
+        self.header = header
+        self.skip_top = skip_top
+        self.skip_bottom = skip_bottom
+        self.file = None
+    def __enter__(self):
+        pass
+    def __exit__(self):
+        self.file.close()
+    def getdata(self):
+        """ Retrieves the data/records from skip_top to skip bottom.
+        Return:
+        nested list (list(list, list, ...)) representing the data.
+        """
+        pass
+    def getheader(self):
+        """ Retrieves the header from csv file.
+        Returns:
+        list: representing the data (when self.header is True).
+        None: (when self.header is False).
+        """
+        pass
