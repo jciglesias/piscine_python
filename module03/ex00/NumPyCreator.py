e8ad74129065abf8a15f84e20724e8e04048c6f8
Juan Carlos <jiglesia@student.42.fr>
2022-12-15 21:39:32 +0100

    protected

--- module03/ex00/NumPyCreator.py
+++ module03/ex00/NumPyCreator.py
@@ -1,18 +1,40 @@
-import numpy, random
+"""Class to create NumPy arrays"""
+import random
+import numpy
+def validate(obj, dtype):
+    """
+    validate list and tuple input
+    """
+    if isinstance(obj, dtype):
+        for i in range(len(obj) - 1):
+            if not isinstance(obj[i], type(obj[i + 1])):
+                return False
+            if isinstance(obj[i], (list, tuple)) and len(obj[i]) != len(obj[i + 1]):
+                return False
+    else:
+        return False
+    return True
 
 class NumPyCreator:
+    """
+    Creates a NumPy array
+    """
     def from_list(self, lst, dtype = None):
         """
         takes a list or nested lists and returns its corresponding
         Numpy array.
         """
-        return numpy.array(lst, dtype=dtype)
+        if validate(lst, list):
+            return numpy.array(lst, dtype=dtype)
+        return None
     def from_tuple(self, tpl, dtype = None):
         """
         takes a tuple or nested tuples and returns its corresponding
         Numpy array.
         """
-        return numpy.array(tpl, dtype=dtype)
+        if validate(tpl, tuple):
+            return numpy.array(tpl, dtype=dtype)
+        return None
     def from_iterable(self, itr, dtype = None):
         """
         takes an iterable and returns an array which contains all
@@ -22,8 +44,10 @@ class NumPyCreator:
             tmp = []
             for i in itr:
                 tmp.append(i)
-            return numpy.array(tmp, dtype=dtype)
-        except Exception:
+            if validate(tmp, list):
+                return numpy.array(tmp, dtype=dtype)
+            return None
+        except TypeError:
             return None
     def from_shape(self, shape, value = 0, dtype = None):
         """
@@ -32,40 +56,46 @@ class NumPyCreator:
         and the second argument specifies the value of the elements.
         This value must be 0 by default.
         """
-        return numpy.full(shape=shape, fill_value=value, dtype=dtype)
+        if len(shape) == 2 and shape[0] >= 0 and shape[1] >= 0:
+            return numpy.full(shape=shape, fill_value=value, dtype=dtype)
+        return None
     def random(self, shape, dtype = None):
         """
         returns an array filled with random values. It takes as an
         argument a tuple which specifies the shape of the array.
         """
         try:
-            y, x, tmp = 0, 0, []
-            while y < shape[0]:
+            _y, _x, tmp = 0, 0, []
+            while _y < shape[0]:
                 tmp.append([])
-                while x < shape[1]:
-                    tmp[y].append(random.random())
-                    x += 1
-                x = 0
-                y += 1
-            return numpy.array(tmp, dtype = dtype)
-        except Exception:
+                while _x < shape[1]:
+                    tmp[_y].append(random.random())
+                    _x += 1
+                _x = 0
+                _y += 1
+            if len(shape) == 2 and shape[0] >= 0 and shape[1] >= 0:
+                return numpy.array(tmp, dtype = dtype)
             return None
-    def identity(self, n, dtype = None):
+        except TypeError:
+            return None
+    def identity(self, _n, dtype = None):
         """
         returns an array representing the identity matrix of size n.
         """
         try:
-            y, x, tmp = 0, 0, []
-            while y < n:
+            _y, _x, tmp = 0, 0, []
+            while _y < _n:
                 tmp.append([])
-                while x < n:
-                    if y == x:
-                        tmp[y].append(1)
+                while _x < _n:
+                    if _y == _x:
+                        tmp[_y].append(1)
                     else:
-                        tmp[y].append(0)
-                    x += 1
-                x = 0
-                y += 1
-            return numpy.array(tmp, dtype = dtype)
-        except Exception:
+                        tmp[_y].append(0)
+                    _x += 1
+                _x = 0
+                _y += 1
+            if _n >= 0:
+                return numpy.array(tmp, dtype = dtype)
+            return None
+        except TypeError:
             return None
--- module03/ex00/test.py
+++ module03/ex00/test.py
@@ -1,14 +1,23 @@
+"""Test for NumPyCreator"""
 from NumPyCreator import NumPyCreator
-import numpy
+import numpy as np
 
 if __name__ == "__main__":
     nc = NumPyCreator()
     tmp = [4, 3, 2, 1]
     tp = (4, 3, 2, 1)
     it = iter(tmp)
-    print(nc.from_list(tmp, dtype=float))
-    print(nc.from_tuple(tp))
-    print(nc.from_iterable(it, dtype=float))
+    print(f"numpy: {np.array(tmp, dtype=float)}\nclass: {nc.from_list(tmp, dtype=float)}")
+    print(f"numpy: {np.array(tp)}\nclass: {nc.from_tuple(tp)}")
+    print(f"numpy: {np.array(tmp, dtype=float)}\nclass: {nc.from_iterable(it, dtype=float)}")
     print(nc.from_shape((3,4), 42))
     print(nc.random((3,4)))
     print(nc.identity(4, dtype=float))
+    print("\nerror management:")
+    print(nc.from_list("toto"))
+    print(nc.from_list([[1,2,3],[6,3,4],[8,5,6,7]]))
+    print(nc.from_tuple(3.2))
+    print(nc.from_tuple(((1,5,8),(7,5))))
+    print(nc.from_shape((-1, -1)))
+    print(nc.from_iterable(1))
+    print(nc.identity(-1))
