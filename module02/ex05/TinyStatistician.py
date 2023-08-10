93bd7975dced3b2df879cf0320a1c96664e89b56
ic-juank <jiglesia@student.42.fr>
2023-08-10 13:28:36 +0200

    shorter code

--- module02/ex05/TinyStatistician.py
+++ module02/ex05/TinyStatistician.py
@@ -7,76 +7,53 @@ class TinyStatistician:
         for-loop. The method returns the mean as a float, otherwise
         None if x is an empty list or array.
         """
-        size = 0
-        if type(x) is list:
-            size = len(x)
-        elif type(x) is numpy.ndarray:
-            size = x.size
-        if size == 0:
-            return None
-        sum = 0
-        for i in x:
-            sum += i    
-        return sum / size
+        if isinstance(x, (numpy.ndarray, list)) and len(x):
+            total = 0
+            for n in x:
+                total += n
+            return float(total / len(x))
+        return None
     def median(self, x: list):
         """
         computes the median of a given non-empty list or array x.
         The method returns the median as a float, otherwise None
         if x is an empty list or array.
         """
-        size = 0
-        if type(x) is list:
-            size = len(x)
-        elif type(x) is numpy.ndarray:
-            size = x.size
-        if size == 0:
-            return None
-        x.sort()
-        return float(x[int(size / 2)])
+        if isinstance(x, (numpy.ndarray, list)) and len(x):
+            tmp = x.copy()
+            tmp.sort()
+            return float(tmp[int(len(tmp) / 2)])
+        return None
     def quartiles(self, x):
         """
         computes the 1st and 3rd quartiles of a given non-empty array x.
         The method returns the quartile as a float, otherwise None if x
         is an empty list or array.
         """
-        size = 0
-        if type(x) is list:
-            size = len(x)
-        elif type(x) is numpy.ndarray:
-            size = x.size
-        if size == 0:
-            return None
-        x.sort()
-        quarter = size / 4
-        return [float(x[int(quarter)]), float(x[int(quarter * 3)])]
+        if isinstance(x, (numpy.ndarray, list)) and len(x):
+            tmp = x.copy()
+            tmp.sort()
+            quarter = len(tmp) / 4
+            return [float(tmp[int(quarter)]), float(tmp[int(quarter * 3)])]
+        return None
     def var(self, x):
         """computes the variance of a given non-empty list or array x,
         using a for-loop. The method returns the variance as a float,
         otherwise None if x is an empty list or array.
         """
-        size = 0
-        if type(x) is list:
-            size = len(x)
-        elif type(x) is numpy.ndarray:
-            size = x.size
-        if size == 0:
-            return None
-        sum = 0
-        mean = self.mean(x)
-        for i in x:
-            sum += (i - mean) * (i - mean)
-        return float(sum / size)
+        if isinstance(x, (numpy.ndarray, list)) and len(x):
+            v = 0
+            m = self.mean(x)
+            for i in x:
+                v += (i - m) * (i - m)
+            return v / len(x)
+        return None
     def std(self, x) :
         """
         computes the standard deviation of a given non-empty list or array
         x, using a for-loop. The method returns the standard deviation as
         a float, otherwise None if x is an empty list or array.
         """
-        size = 0
-        if type(x) is list:
-            size = len(x)
-        elif type(x) is numpy.ndarray:
-            size = x.size
-        if size == 0:
-            return None
-        return math.sqrt(self.var(x))
+        if isinstance(x, (numpy.ndarray, list)) and len(x):
+            return math.sqrt(self.var(x))
+        return None
