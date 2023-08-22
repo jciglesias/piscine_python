ea187ea65e0d8f6b1a85e07400e2e352e267bc56
ic-juank <jiglesia@student.42.fr>
2023-08-22 12:16:26 +0200

    ColorFilter

--- module03/ex03/ColorFilter.py
+++ module03/ex03/ColorFilter.py
@@ -1,5 +1,17 @@
+import numpy as np
 
 class ColorFilter:
+    """
+    A tool that can apply a variety of color filters on images.
+    methods:
+        invert
+        to_blue
+        to_green
+        to_red
+        to_celluloid
+        to_grayscale
+    """
+
     def invert(self, array):
         """
         Inverts the color of the image received as a numpy array.
@@ -14,6 +26,9 @@ class ColorFilter:
         -------
             This function should not raise any Exception.
         """
+        if isinstance(array, np.ndarray):
+            return 1 - array 
+        return None
 
     def to_blue(self, array):
         """
@@ -29,6 +44,15 @@ class ColorFilter:
         -------
             This function should not raise any Exception.
         """
+        if isinstance(array, np.ndarray):
+            arr = array.copy()
+            for row in arr:
+                for column in row:
+                    column[0] *= 0.5
+                    column[1] *= 0.5
+                    # column[2]
+            return arr
+        return None
 
     def to_green(self, array):
         """
@@ -44,6 +68,15 @@ class ColorFilter:
         -------
             This function should not raise any Exception.
         """
+        if isinstance(array, np.ndarray):
+            arr = array.copy()
+            for row in arr:
+                for column in row:
+                    column[0] *= 0.5
+                    # column[1]
+                    column[2] *= 0.5
+            return arr
+        return None
 
     def to_red(self, array):
         """
@@ -59,6 +92,15 @@ class ColorFilter:
         -------
             This function should not raise any Exception.
         """
+        if isinstance(array, np.ndarray):
+            arr = array.copy()
+            for row in arr:
+                for column in row:
+                    # column[0]
+                    column[1] *= 0.5
+                    column[2] *= 0.5
+            return arr
+        return None
 
     def to_celluloid(self, array):
         """
@@ -70,32 +112,55 @@ class ColorFilter:
         celluloid filter is also known as cel-shading or toon-shading.
         Args:
         -----
-        array: numpy.ndarray corresponding to the image.
+            array: numpy.ndarray corresponding to the image.
         Return:
         -------
-        array: numpy.ndarray corresponding to the transformed image.
-        None: otherwise.
+            array: numpy.ndarray corresponding to the transformed image.
+            None: otherwise.
         Raises:
         -------
-        This function should not raise any Exception.
+            This function should not raise any Exception.
         """
+        if isinstance(array, np.ndarray):
+            arr = array.copy()
+            for row in arr:
+                for column in row:
+                    if np.sum(column) < 1.0:
+                        column *= 0.5
+            return arr
+        return None
 
     def to_grayscale(self, array, filter, **kwargs):
         """
         Applies a grayscale filter to the image received as a numpy array.
-        For filter = 'mean'/'m': performs the mean of RBG channels.
-        For filter = 'weight'/'w': performs a weighted mean of RBG channels.
+            For filter = 'mean'/'m': performs the mean of RBG channels.
+            For filter = 'weight'/'w': performs a weighted mean of RBG channels.
         Args:
         -----
-        array: numpy.ndarray corresponding to the image.
-        filter: string with accepted values in ['m','mean','w','weight']
-        weights: [kwargs] list of 3 floats where the sum equals to 1,
-        corresponding to the weights of each RBG channels.
+            array: numpy.ndarray corresponding to the image.
+            filter: string with accepted values in ['m','mean','w','weight']
+            weights: [kwargs] list of 3 floats where the sum equals to 1,
+            corresponding to the weights of each RBG channels.
         Return:
         -------
-        array: numpy.ndarray corresponding to the transformed image.
-        None: otherwise.
+            array: numpy.ndarray corresponding to the transformed image.
+            None: otherwise.
         Raises:
         -------
-        This function should not raise any Exception.
+            This function should not raise any Exception.
         """
+        if isinstance(array, np.ndarray) and filter in ['m','mean','w','weight']:
+            arr = array.copy()
+            for row in arr:
+                for column in row:
+                    if filter in ['m', 'mean']:
+                        tmp = np.sum(column)
+                        column[0] = tmp / 3
+                        column[1] = tmp / 3
+                        column[2] = tmp / 3
+                    else:
+                        column[0] *= kwargs[filter][0]
+                        column[1] *= kwargs[filter][1]
+                        column[2] *= kwargs[filter][2]
+            return arr
+        return None
--- module03/ex03/test.py
+++ module03/ex03/test.py
@@ -1,14 +1,38 @@
-from ImageProcessor import ImageProcessor
-imp = ImageProcessor()
-arr = imp.load("assets/42AI.png")
-# Output :
-# Loading image of dimensions 200 x 200
+import numpy as np
+from matplotlib import pyplot as plt
+
 from ColorFilter import ColorFilter
 cf = ColorFilter()
-cf.invert(arr)
-cf.to_green(arr)
-cf.to_red(arr)
-cf.to_blue(arr)
-cf.to_celluloid(arr)
-cf.to_grayscale(arr, 'm')
-cf.to_grayscale(arr, 'weight', weights = [0.2, 0.3, 0.5])
+
+# for f in [cf.to_red, cf.to_green, cf.to_blue, cf.invert]:
+arr = plt.imread("assets/42AI.png")
+plt.title("Original")
+plt.imshow(arr)
+plt.show()
+	# plt.imshow(f(arr))
+	# plt.show()
+# Output :
+# Loading image of dimensions 200 x 200
+
+# print(arr.shape)
+plt.title("Invert")
+plt.imshow(cf.invert(arr))
+plt.show()
+plt.title("Green")
+plt.imshow(cf.to_green(arr))
+plt.show()
+plt.title("Red")
+plt.imshow(cf.to_red(arr))
+plt.show()
+plt.title("Blue")
+plt.imshow(cf.to_blue(arr))
+plt.show()
+plt.title("Celluloid")
+plt.imshow(cf.to_celluloid(arr))
+plt.show()
+plt.title("Grayscale")
+plt.imshow(cf.to_grayscale(arr, 'm'), cmap="gray")
+plt.show()
+plt.title("Grayscale weight")
+plt.imshow(cf.to_grayscale(arr, 'weight', weight = [0.2, 0.3, 0.5]), cmap="gray")
+plt.show()
