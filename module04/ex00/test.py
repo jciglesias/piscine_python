134bf9f940c1a8d5f0d1037c04d378fbdb9c7e03
ic-juank <jiglesia@student.42.fr>
2023-08-28 11:28:04 +0200

    display 0

--- module04/ex00/test.py
+++ module04/ex00/test.py
@@ -6,21 +6,19 @@ data = loader.load("data/athlete_events.csv")
 # (271116, 15) 
 print("Example 1:")
 loader.display(data, 3)
-# Output
 #    ID                 Name Sex   Age  Height  Weight     Team  NOC        Games  Year  Season       City       Sport                         Event Medal
 # 0   1            A Dijiang   M  24.0   180.0    80.0    China  CHN  1992 Summer  1992  Summer  Barcelona  Basketball   Basketball Men's Basketball   NaN
 # 1   2             A Lamusi   M  23.0   170.0    60.0    China  CHN  2012 Summer  2012  Summer     London        Judo  Judo Men's Extra-Lightweight   NaN
 # 2   3  Gunnar Nielsen Aaby   M  24.0     NaN     NaN  Denmark  DEN  1920 Summer  1920  Summer  Antwerpen    Football       Football Men's Football   NaN
-print("Example 2:")
+print("\nExample 2:")
 loader.display(data, -3)
-# Output
 # ID                Name Sex   Age  Height  Weight    Team  NOC        Games  Year  Season            City        Sport                               Event Medal
 # 271113  135570            Piotr ya   M  27.0   176.0    59.0  Poland  POL  2014 Winter  2014  Winter           Sochi  Ski Jumping  Ski Jumping Men's Large Hill, Team   NaN
 # 271114  135571  Tomasz Ireneusz ya   M  30.0   185.0    96.0  Poland  POL  1998 Winter  1998  Winter          Nagano    Bobsleigh                Bobsleigh Men's Four   NaN
 # 271115  135571  Tomasz Ireneusz ya   M  34.0   185.0    96.0  Poland  POL  2002 Winter  2002  Winter  Salt Lake City    Bobsleigh                Bobsleigh Men's Four   NaN
-print("Example 3:")
-loader.display(data, -3)
+print("\nExample 3:")
+loader.display(data, 0)
 # should display Nothing or the Header (column names of the dataframe)
-print("Example 4:")
+print("\nExample 4:")
 loader.display(data, "lol")
 #shouldnt crash
