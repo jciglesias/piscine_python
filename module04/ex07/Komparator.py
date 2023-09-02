1dacc1fe0d914928a58c087ff204239086ceb9fc
Juan Carlos Iglesias <jiglesia@student.42.fr>
2023-09-02 14:30:44 +0200

    histograms

--- module04/ex07/Komparator.py
+++ module04/ex07/Komparator.py
@@ -50,3 +50,13 @@ class Komparator:
         for each category. As an extra, you can use overlapping
         histograms with a color code.
         """
+        cat = self.data[categorical_var]
+        num = self.data[numerical_var]
+        x = list({i for i in cat if i is not np.nan})
+        fig, ax = plt.subplots(1,len(x))
+        for i in range(len(x)):
+            sns.histplot([num[a] for a in range(len(num)) if ~np.isnan(num[a]) and cat[a] == x[i]], ax=ax[i])
+            # ax[i].displot([num[a] for a in range(len(num)) if ~np.isnan(num[a]) and cat[a] == x[i]], kind="kde")
+            ax[i].set_xlabel(x[i])
+            ax[i].set_ylabel(numerical_var)
+        plt.show()
